ba6998acf451e2294c4beb3db943c86c12b2eb2f
Fzzrr <farhanziarizky@gmail.com>
2026-04-06 12:18:23 +0700

    feat: complete reset password page

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -5,14 +5,6 @@ type Props = {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
-export default async function ResetPasswordPage({ searchParams }: Props) {
-  const params = await searchParams;
-  const error = typeof params.error === 'string' ? params.error : undefined;
-  const token = typeof params.token === 'string' ? params.token : undefined;
-
-  return (
-    <main className="flex min-h-screen items-center justify-center p-4 bg-background">
-      <ResetPasswordForm token={token} error={error} />
-    </main>
-  );
+export default function ResetPasswordPage() {
+  return <ResetPasswordForm />;
 }
