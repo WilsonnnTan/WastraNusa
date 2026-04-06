b5550b4a290cca64a02f33c509cb885802e3dcc9
Fzzrr <farhanziarizky@gmail.com>
2026-04-06 12:27:36 +0700

    feat: implement auth pages

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import { AuthLayout, RegisterForm } from '@/components/auth';
+
+export default function RegisterPage() {
+  return (
+    <AuthLayout>
+      <RegisterForm />
+    </AuthLayout>
+  );
+}
