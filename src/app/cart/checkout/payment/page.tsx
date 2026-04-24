ef535453d1e52729099debc793a1795f8e7ebd06
WilsonnnTan <wilsonangelietann@gmail.com>
2026-04-24 10:13:04 +0700

    refactor: remove useless metadata and redirect query params

--- src/app/cart/checkout/payment/page.tsx
+++ src/app/cart/checkout/payment/page.tsx
@@ -1,19 +1,7 @@
 import { PaymentMain } from '@/components/checkout/payment/payment-main';
 import { requireUser } from '@/lib/auth/auth-page-helper';
-import { Metadata } from 'next';
-import { redirect } from 'next/navigation';
-
-export const metadata: Metadata = {
-  title: 'Konfirmasi Pembayaran - Wastra Nusa',
-  description: 'Tinjau dan konfirmasi pesanan Wastra Anda.',
-};
 
 export default async function PaymentPage() {
-  try {
-    await requireUser();
-  } catch {
-    redirect('/login?callbackUrl=/cart/checkout/payment');
-  }
-
+  await requireUser();
   return <PaymentMain />;
 }
